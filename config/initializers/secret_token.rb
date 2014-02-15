0aaa2cff021c0e1b847082240ff0fe8cb413f345
Andrew Butterfield <andrewcbutterfield@gmail.com>
2014-02-15 10:53:48 -0700

    Really added ruby on rails to zuse web.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '13fbfad14b74c7b8d8edcd75673b0648e4f08d51524c3bf3c824de25f9c9a3097964daa0984f9364b6009ed8ffd938b1ec483d95dbd2d3f2e7819464acb84982'
