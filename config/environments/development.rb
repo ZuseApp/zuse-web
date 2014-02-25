90c1b1aa632111720126e2e4081174a20945a62c
Andrew Butterfield <andrewcbutterfield@gmail.com>
2014-02-25 12:49:27 -0700

    Trying again.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Myapp::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.assets.precompile += %w( shared_projects.js.coffee )
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
